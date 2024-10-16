5ade4765f2018510c33fbcce879f60abf5acdbf7
Gowshik <koushikraj60@gmail.com>
2024-10-16 14:03:40 +0530

    updateProdcuctImage

--- sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -51,6 +54,13 @@ public class ProductController {
     @DeleteMapping("/admin/deleteProducts/{productId}")
     public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
         ProductDTO productDTO = productService.deleteProduct(productId);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/products/{productId}/image")
+    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
+                                                         @RequestParam("image") MultipartFile image) throws IOException {
+        ProductDTO productDTO = productService.updateProductImage(productId, image);
         return new ResponseEntity<>(productDTO, HttpStatus.OK);
     }
 }
--- sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -11,8 +11,16 @@ import com.ecommerce.project.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -111,5 +119,46 @@ public class ProductServiceImpl implements ProductService {
 
         productRepository.delete(product);
         return modelMapper.map(product, ProductDTO.class);
+    }
+
+    @Override
+    public ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException {
+        Product product = productRepository.findById(productId).
+                orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
+        String path = "images";
+        String fileName = uploadServer(path, image);
+
+        // Updating the new file name to the product
+        product.setImage(fileName);
+        // Save updated product
+        Product updatedProduct = productRepository.save(product);
+        // return DTO after mapping product to DTO
+        return modelMapper.map(updatedProduct, ProductDTO.class);
+    }
+
+    private String uploadServer(String path, MultipartFile image) throws IOException {
+        String originalFileName = image.getOriginalFilename();
+
+
+        //Generate a unique filename
+        String randomID = UUID.randomUUID().toString();
+        // mat.jpg --> 1234 --> 1234.jpg
+        String fileName = randomID.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        String filePath = path + File.separator +  fileName;
+
+        File file = new File(path);
+        if (!file.exists()) {
+            if (file.mkdir()) {
+                System.out.println("Directory created: " + file.getAbsolutePath());
+            } else {
+                System.out.println("Failed to create directory: " + file.getAbsolutePath());
+            }
+        }
+
+
+        Files.copy(image.getInputStream(), Paths.get(filePath));
+
+        return fileName;
     }
 }
